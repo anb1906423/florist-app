e5ee7fde47e54910b8b9a46c755e86f73ac0f0cb
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-06-02 14:13:57 +0700

    order detail screen

--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/drawer.dart';
+import 'order_detail.dart';
 
 class Orders extends StatefulWidget {
   const Orders({super.key});
@@ -17,7 +18,7 @@ class _OrderState extends State<Orders> {
         title: const Text('Đơn hàng'),
         backgroundColor: Colors.pink[100],
       ),
-      drawer:  const AppDrawer(),
+      drawer: const AppDrawer(),
       body: SingleChildScrollView(
         physics: const AlwaysScrollableScrollPhysics(),
         child: Padding(
@@ -29,7 +30,13 @@ class _OrderState extends State<Orders> {
               const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("DANH SÁCH ĐƠN HÀNG", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, fontFamily: AutofillHints.addressCity),)
+                  Text(
+                    "DANH SÁCH ĐƠN HÀNG",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: AutofillHints.addressCity),
+                  )
                 ],
               ),
               ListView.builder(
@@ -37,66 +44,76 @@ class _OrderState extends State<Orders> {
                 itemCount: 2,
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
-                  return Container(
-                    padding: const EdgeInsets.all(10),
-                    margin: const EdgeInsets.only(
-                      top: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Color.fromARGB(255, 208, 207, 207),
-                          offset: Offset(0.0, 3.0),
-                        ),
-                      ],
-                    ),
-                    child: const Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 10),
-                          child: Row(
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => OrderDetailScreen(
+                                orderId: '6470a2d977a4de6a69d88597')),
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.only(
+                        top: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color.fromARGB(255, 208, 207, 207),
+                            offset: Offset(0.0, 3.0),
+                          ),
+                        ],
+                      ),
+                      child: const Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 10),
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  "Hoa hồng nữ hoàng",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: AutofillHints.addressCity),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "Hoàn thành",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.green,
+                                      fontFamily: AutofillHints.addressCity),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Row(
                             children: <Widget>[
                               Text(
-                                "Hoa hồng nữ hoàng",
+                                "23/5/2023 2:05:05 pm",
                                 style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
                                     fontFamily: AutofillHints.addressCity),
                               ),
                               Spacer(),
                               Text(
-                                "Hoàn thành",
+                                "31.06\$",
                                 style: TextStyle(
                                     fontSize: 18,
                                     fontWeight: FontWeight.w500,
-                                    color: Colors.green,
                                     fontFamily: AutofillHints.addressCity),
                               ),
                             ],
                           ),
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "23/5/2023 2:05:05 pm",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: AutofillHints.addressCity),
-                            ),
-                            Spacer(),
-                            Text(
-                              "31.06\$",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500,
-                                  fontFamily: AutofillHints.addressCity),
-                            ),
-                          ],
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   );
                 },
